d2fd320b8337967332990f6bf456997f4f3daf53
Rory Campbell <roryc89@googlemail.com>
2024-09-18 01:03:35 +0200

    connecting to purs lsp server

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,121 @@
+import { workspace, ExtensionContext, window, TextDocument } from 'vscode';
 import {
-	Executable,
+	CloseAction,
+	ErrorAction,
 	LanguageClient,
 	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
+	RevealOutputChannelOn,
+	ServerOptions
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
+	const name = 'purescript-lsp';
+
+	console.log('Congratulations, "purecript-lsp" is now active!');
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	const serverOptions: ServerOptions = {
-		run: {
-			command: "purs lsp",
-			transport: TransportKind.ipc
-		},
-		debug: {
-			command: "purs lsp",
-			transport: TransportKind.ipc
-		}
+		command: "/Users/rorycampbell/.local/bin/purs",
+		args: ["lsp", "server"],
 	};
+
+	const outputChannel = window.createOutputChannel(name);
 
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'purescript' }],
+		documentSelector: [
+			{
+				scheme: 'file',
+				language: 'purescript',
+				pattern: '**/*.purs'
+			}],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
+		},
+		outputChannel,
+		diagnosticCollectionName: name,
+		outputChannelName: name,
+		revealOutputChannelOn: RevealOutputChannelOn.Never,
+		errorHandler: {
+			error: (e, m, c) => { console.error(e, m, c); return { action: ErrorAction.Continue }; },
+			closed: () => ({ action: CloseAction.DoNotRestart })
+		},
+		initializationOptions: {
+			executeCommandProvider: false
+		},
 	};
+
+
 
 	// Create the language client and start the client.
 	client = new LanguageClient(
-		'OAPurescriptLanguageServer',
-		'Oxford Abstracts PureScript Language Server',
+		'purescript',
+		name,
 		serverOptions,
 		clientOptions
 	);
 
+	client.onNotification('textDocument/publishDiagnostics', (params) => {
+		console.log('publishDiagnostics', params);
+		// workspace.
+	});
+
+	client.onNotification('textDocument/diagnosticsBegin', (params) => {
+		console.log('diagnosticsBegin', params);
+	});
+	client.onNotification('textDocument/diagnosticsEnd', (params) => {
+		console.log('diagnosticsEnd', params);
+	});
+
+	client.onNotification('textDocument/typeCoverage', (params) => {
+		console.log('typeCoverage', params);
+	});
+
+	client.onNotification('textDocument/cleanBegin', (params) => {
+		console.log('cleanBegin', params);
+	});
+
+	client.onNotification('textDocument/cleanEnd', (params) => {
+		console.log('cleanEnd', params);
+	});
+
+
+	workspace.onDidOpenTextDocument(async (document: TextDocument) => {
+		console.log('onDidOpenTextDocument', document.languageId);
+		if (document.languageId === 'purescript') {
+			console.log('onDidOpenTextDocument', document.uri);
+			const params = {
+				textDocument: {
+					uri: document.uri.toString(),
+				},
+			};
+			client.sendNotification('textDocument/didOpen', params);
+		}
+	});
+
+	workspace.onDidSaveTextDocument(async (document: TextDocument) => {
+		console.log('onDidSaveTextDocument', document.languageId);
+
+		if (document.languageId === 'purescript') {
+			console.log('onDidSaveTextDocument', document.uri);
+			const params = {
+				textDocument: {
+					uri: document.uri.toString(),
+				},
+			};
+			client.sendNotification('textDocument/didSave', params);
+		}
+	});
+
+
+	client.registerProposedFeatures();
 	// Start the client. This will also launch the server
-	client.start();
+	await client.start();
+
 }
 
 export function deactivate(): Thenable<void> | undefined {
